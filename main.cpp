516c3a1efedc364a28341e31c620108605fe76c5
Mg-Sai <drizzmece@gmail.com>
2025-09-27 10:21:05 +0630

    added functions

--- main.cpp
+++ main.cpp
@@ -1,12 +1,43 @@
 #include <iostream>
-
+#include <fstream>
+#include <sstream>
+#include <vector>
 #include <string>
+
+struct date {
+    int day, month, year;
+};
+
+struct transaction {
+    std::string name;
+    int ammount;
+    date date;
+    std::string catagory;
+};
 
 using namespace std;
 
 int main(){
+//format date 
+string formatDate(const date& d) {
+    ostringstream ss;
+    ss << (d.day< 10 ? "0" : "") << d.day << "-" << (d.month < 10 ? "0" : "") << d.month << "-" << d.year;
+    return ss.str();
+}
 
-    int choice;
+//Save to custom file
+void  savetran(const vector<transactions>& transactions, const string &filename) {
+
+    ofstream out(filename);
+    for (const auto &t : transactions) {
+        out << "NAME:" << t.name
+            << "AMMOUNT:" << t.ammount
+            << "DATE:" << formatDate(t.date)
+            << "CATEGORY:" << t.catagory << endl;
+    }
+    
+}
+
     cout << "HELLO TO THE TRANSCATION MANAGER" << endl;
     cout << "--------------------------------" << endl;
     cout << " " << endl;
@@ -18,8 +49,33 @@ int main(){
     cout << "4. EXIT" << endl;
     cout << "PLEASE TYPE THE NUMBER OF YOUR CHOICE --> " << endl; cin >> choice;
 
-    if(choice == 1){
-        cout << "CREATE A TRANSACTION" << endl;
+    if(choice >= 1 && choice <= 4){
+        choicebool = true;
+    }
+    else{
+        cout << "INVALID INPUT" << endl;
+        return 0;
+    }
+
+    switch (choice)
+    {
+        case 1:
+            cout << "CREATE A TRANSACTION" << endl;
+            cout << "---------------------" << endl;
+            cout << "PLEASE TYPE THE NAME OF THE TRANSACTION:", cin >> nametrans;
+            cout << "AMMOUNT:", cin >> ammount;
+            cout << "DATE:", cin >> date;
+            cout << "CATEGORY:", cin >> datecatagory; 
+            break;
+        case 2:
+            cout << "VIEW A TRANSACTION" << endl;
+            break;
+        case 3:
+            cout << "DELETE A TRANSACTION" << endl;
+            break;
+        case 4:
+            cout << "EXIT" << endl;
+            break;
     }
 
     return 0;
