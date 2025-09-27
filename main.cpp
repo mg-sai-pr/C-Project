0c580c5e1b280d949c597e84a36a7c98ede332e2
Mg-Sai <drizzmece@gmail.com>
2025-09-27 09:48:56 +0630

    added gui libs

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+
+#include <string>
+
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
+int main(){
+
+    int choice;
+    cout << "HELLO TO THE TRANSCATION MANAGER" << endl;
+    cout << "--------------------------------" << endl;
+    cout << " " << endl;
+    cout << "PLEASE CHOOSE ONE OF THE FOLLOWING OPTIONS" << endl;
+    cout << "--------------------------------" << endl;
+    cout << "1. CREATE A TRANSACTION" << endl;
+    cout << "2. VIEW A TRANSACTION" << endl;
+    cout << "3. DELETE A TRANSACTION" << endl;
+    cout << "4. EXIT" << endl;
+    cout << "PLEASE TYPE THE NUMBER OF YOUR CHOICE --> " << endl; cin >> choice;
+
+    if(choice == 1){
+        cout << "CREATE A TRANSACTION" << endl;
+    }
+
     return 0;
 }
